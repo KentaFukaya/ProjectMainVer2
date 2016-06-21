a07b169886d1870c6a759b5ed13a0f1e8d3c8c53
b1014100 <b1014100@fun.a.cjp>
2016-06-21 12:53:39 +0900

    quiz画面のサンプルファイルを複数追加

---
+++ app/src/main/java/xyz/virtual_diving/projectmainver2/Quiz/QuizActivity.java
@@ -0,0 +1,26 @@
+package xyz.virtual_diving.projectmainver2.Quiz;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import xyz.virtual_diving.projectmainver2.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quizactivity_main);
+
+        //画面表示
+        setViews();
+    }
+
+    private void setViews(){
+        FragmentManager manager = getSupportFragmentManager();
+        QuizViewPager viewPager = (QuizViewPager)findViewById(R.id.quiz_viewpager);
+        QuizFragmentPagerAdapter adapter = new QuizFragmentPagerAdapter(manager);
+        viewPager.setAdapter(adapter);
+    }
+}

d06f41547a02144cd5fb3b8838af5f06e1e24f51
b1014169 <b1014169@fun.ac.jp>
2016-06-26 07:22:24 +0900

    Imageパッケージを追加 ImageパッケージにImageActivityを追加 activity_image.xmlを追加

---
+++ app/src/main/java/xyz/virtual_diving/projectmainver2/Image/ImageActivity.java
@@ -0,0 +1,15 @@
+package xyz.virtual_diving.projectmainver2.Image;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xyz.virtual_diving.projectmainver2.R;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}

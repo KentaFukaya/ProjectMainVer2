461da98e0e855b7615e3c887213b9c9fe75d3829
b1014100 <b1014100@fun.a.cjp>
2016-06-26 14:33:04 +0900

    [Zukanlist/ZukanlistActivity] update DB

--- app/src/main/java/xyz/virtual_diving/projectmainver2/ZukanList/ZukanListActivity.java
+++ app/src/main/java/xyz/virtual_diving/projectmainver2/ZukanList/ZukanListActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.ArrayList;
 import xyz.virtual_diving.projectmainver2.DB.ZukanDatabase;
@@ -16,9 +17,9 @@ import xyz.virtual_diving.projectmainver2.R;
 public class ZukanListActivity extends AppCompatActivity {
     public static ArrayList<ZukanListItem> Zukanitems = new ArrayList<>();
     private static Context ctx;
-    private String ZukanFishName[];
-    private String ZukanAbstract[];
-    private String ZukanContents[] [];
+    private String ZukanFishName[] = new String [4];
+    private String ZukanAbstract[] = new String [4];
+    private String ZukanContents[] [] = new String[4][3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,8 +40,7 @@ public class ZukanListActivity extends AppCompatActivity {
     }
 
     //データベースに追加するための配列のデータを入れる
-    public  void setZuknaContents(){
-
+    public  void setZukanContents(){
         //page0
         ZukanFishName[0] = "マアジ";
         ZukanAbstract[0] = "スズキ目アジ科";
@@ -63,28 +63,25 @@ public class ZukanListActivity extends AppCompatActivity {
         ZukanContents[2] [2] = "代表的な調理方法は\n刺身、寿司、ステーキ、缶詰など幅広い。\n日本ではかなり昔から食べられている。";
 
         //page2
-        ZukanFishName[2] = "マダイ";
-        ZukanAbstract[2] = "スズキ目タイ科\n";
-        ZukanContents[2] [0] = "日本列島全域に分布。\n大きさは120cmと比較的大きい。";
-        ZukanContents[2] [1] = "旬は1〜3月ごろ。\n頑丈な顎と歯でエビやカニなど固い殻でも噛み砕いて食べる。";
-        ZukanContents[2] [2] = "代表的な調理方法は\n刺身、焼き魚、吸い物、煮付けなど多種多様。\n日本では昔から目出度い魚だとされている。";
+        ZukanFishName[3] = "マダイ";
+        ZukanAbstract[3] = "スズキ目タイ科\n";
+        ZukanContents[3] [0] = "日本列島全域に分布。\n大きさは120cmと比較的大きい。";
+        ZukanContents[3] [1] = "旬は1〜3月ごろ。\n頑丈な顎と歯でエビやカニなど固い殻でも噛み砕いて食べる。";
+        ZukanContents[3] [2] = "代表的な調理方法は\n刺身、焼き魚、吸い物、煮付けなど多種多様。\n日本では昔から目出度い魚だとされている。";
 
     }
     //データベースに追加する
     public void setItemsfromDB(){
+        setZukanContents();
+
         String contents[] = new String[3];
         contents[0] = "０番目の詳しい内容\n関係ないけどスワイプもボタンタッチでも次のページに行けるよ\n左の矢印は消してるよ";
         contents[1] = "1番目の詳しい内容\n魚に詳しくないので名に書いたらいいかわかりませんうまくいくといいな";
         contents[2] = "2番目の詳しい内容\n最後のぺーじ、どんなクイズができているんでしょう？\n右の矢印は消してるよ";
-        for(int i = 0 ; i < 13 ; i++) {
-            if (i % 3 == 0) {//画像入れてるだけ
-                ZukanDatabase.setZukanData(i,i%3,"魚", "魚の説明　id ="+i,contents);
-            } else if (i % 3 == 1) {
-                ZukanDatabase.setZukanData(i,i%3,"クリオネ", "魚の説明　id ="+i,contents);
-            } else {
-                ZukanDatabase.setZukanData(i,i%3,"クラゲ", "魚の説明　id ="+i,contents);
-            }
+        for(int i = 0 ; i < ZukanFishName.length ; i++) {
+            ZukanDatabase.setZukanData(i,i,ZukanFishName[i],ZukanAbstract[i],ZukanContents[i]);
         }
+
     }
     //データベースの情報をとってくる。
     public void getItemsformDB(){

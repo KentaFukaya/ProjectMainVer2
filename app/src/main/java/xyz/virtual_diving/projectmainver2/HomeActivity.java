a5928e8432355201eb5c77cbb01b060b6609766b
Yuna Tanaka <b1014183@fun.ac.jp>
2016-06-26 15:05:24 +0900

    ・HomeActivityにあったテスト用のコメントの削除 ・タイトル画面用の背景用画像（homeactivity_background.jpg）を追加 以上2点行いました。 まだ背景に設定はしていません。

--- app/src/main/java/xyz/virtual_diving/projectmainver2/HomeActivity.java
+++ app/src/main/java/xyz/virtual_diving/projectmainver2/HomeActivity.java
@@ -24,7 +24,6 @@ public class HomeActivity extends Activity {
             public  void onClick(View v){
                 Intent intent = new Intent(HomeActivity.this, MovieListActivity.class); //ダイビングアクティビティに飛ぶ処理
                 startActivity(intent);
-                //test_10th
             }
         });
 

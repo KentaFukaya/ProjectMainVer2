052b36e7da95633ab12509ab9e094c6d7f72ffa7
Yuna Tanaka <b1014183@fun.ac.jp>
2016-07-03 16:20:03 +0900

    ・insea.mp4の追加 ・動画をinsea.mp4に差し替え 以上2つ行いました。 海中の動画ですので、画面酔いにお気を付けください。

--- app/src/main/java/xyz/virtual_diving/projectmainver2/panframe/SimplePlayerActivity.java
+++ app/src/main/java/xyz/virtual_diving/projectmainver2/panframe/SimplePlayerActivity.java
@@ -248,7 +248,7 @@ public class SimplePlayerActivity extends FragmentActivity implements PFAssetObs
     private OnClickListener playListener = new OnClickListener() {
         public void onClick(View v) {
             if (_pfasset == null)
-                loadVideo("android.resource://" + getPackageName() + "/" + R.raw.sakana2);
+                loadVideo("android.resource://" + getPackageName() + "/" + R.raw.insea);
 
             if (_pfasset.getStatus() == PFAssetStatus.PLAYING) {
                 PushPlayButtonTime = _pfasset.getPlaybackTime();
